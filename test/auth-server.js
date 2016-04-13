4e5fd10bb2e39a6b87dc25c89a85e1531ac5ed1b
Rumkin <dev@rumk.in>
2016-04-13 14:03:20 +0000

    create test authentication server

---
+++ test/auth-server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const http = require('http');
+const port = process.argv[2] || 8080;
+
+http.createServer((req, res) => {
+    // Read body
+    if (req.method === 'GET') {
+        res.end('{}');
+        return;
+    }
+
+    let chunks = [];
+    let json = function(data) {
+        var out = JSON.stringify(data);
+        res.setHeader('content-type', 'application/json');
+        res.setHeader('content-length', out.length);
+        res.end(out);
+    }
+
+    if (! req.headers['content-length']) {
+        res.statusCode = 400;
+        json({error: 'empty_body'});
+        return;
+    }
+
+    req.on('data', chunk => {
+        chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+        var data = chunks.map(chunk => chunk.toString()).join('');
+
+        try {
+            data = JSON.parse(data);
+        } catch (err) {
+            res.statusCode = 400;
+            json({error: 'not_a_json'});
+            return;
+        }
+
+        if (typeof data !== 'object') {
+            res.statusCode = 400;
+            json({error: 'json_body'});
+            return;
+        }
+
+        if (data.user !== 'user' || data.signature !== 'signature') {
+            res.statusCode = 403;
+            json({result: false});
+            return;
+        }
+
+        json({result: true});
+    });
+})
+.listen(port, () => {
+    console.log('Server is started at 0.0.0.0:%s', port);
+});
